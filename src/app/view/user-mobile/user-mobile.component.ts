532e539f7e06cf2bb53a94e56cf2aeda99656eee
Calvin-Wijaya <67195371+Calvin-Wijaya@users.noreply.github.com>
2022-07-11 18:58:16 +0700

    update

--- src/app/view/user-mobile/user-mobile.component.ts
+++ src/app/view/user-mobile/user-mobile.component.ts
@@ -3,13 +3,10 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-user-mobile',
   templateUrl: './user-mobile.component.html',
-  styleUrls: ['./user-mobile.component.scss']
+  styleUrls: ['./user-mobile.component.scss'],
 })
 export class UserMobileComponent implements OnInit {
+  constructor() {}
 
-  constructor() { }
-
-  ngOnInit(): void {
-  }
-
+  ngOnInit(): void {}
 }
